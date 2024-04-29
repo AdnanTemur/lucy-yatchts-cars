ee909a83725db169454149b64889209dd144a3a7
AdnanTemur <adt.gb001@gmail.com>
2024-04-29 22:14:33 +0500

    all pages done + loader alpinejs

---
+++ resources/views/components/preloader.blade.php
@@ -0,0 +1,10 @@
+<div x-show="loaded" x-init="window.addEventListener('DOMContentLoaded', () => { setTimeout(() => loaded = false, 600) })">
+    <div class="w-100 d-flex align-items-center justify-content-center position-absolute"
+        style="z-index: 200; background: white;height: 100vh;">
+        <div class="w-100 d-flex align-items-center justify-content-center">
+            <div class="spinner-border" role="status"
+                style="color: #050b20; height: 3rem; width: 3rem; font-size: 1.5rem;">
+            </div>
+        </div>
+    </div>
+</div>
